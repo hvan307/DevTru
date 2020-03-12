88d95b0cedea2d0164d811c5cb353d4417b310c6
Finlay Whittington-Devereux <fin.whittington1@gmail.com>
2020-03-12 18:46:53 +0000

    started styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -31,23 +31,48 @@ class NavBar extends React.Component {
   }
 
   render() {
-    console.log(this.state.search)
-    return <form onSubmit={(event) => this.handleSubmit(event)}>
-      <div className="control is-expanded">
-        <input
-          onChange={(event) => this.handleChange(event)}
-          // onSubmit={(event => this.handleSubmit(event))}
-          className="input is-medium"
-          type="search"
-          placeholder="Artist, Album, Song"
-          value={this.state.search}
-        />
+    return <nav class="navbar">
+      <div class="navbar-brand">
+        <Link class="navbar-item" to="/">
+          <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
+        </Link>
+
+        <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
       </div>
-      <div className="control">
-        {/* <button type="submit"></button> */}
-        {/* <Link to={{ pathname: '/search', state: this.state.search }}>submit</Link> */}
+
+      <div class="navbar-menu">
+        <div class="navbar-start">
+          <Link class="navbar-item" to="/">Home</Link>
+          <Link class="navbar-item" to="/">Charts</Link>
+
+
+          <div class="navbar-item">
+            <form onSubmit={(event) => this.handleSubmit(event)}>
+              <div className="control is-expanded">
+                <input
+                  onChange={(event) => this.handleChange(event)}
+                  // onSubmit={(event => this.handleSubmit(event))}
+                  className="input is-medium"
+                  type="search"
+                  placeholder="Artist, Album, Song"
+                  value={this.state.search}
+                />
+              </div>
+              <div className="control">
+                {/* <button type="submit"></button> */}
+                {/* <Link to={{ pathname: '/search', state: this.state.search }}>submit</Link> */}
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
-    </form>
+
+    </nav >
+
   }
 
 }
