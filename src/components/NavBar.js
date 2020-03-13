207f20bece2004b4e9facec7b81908c173b68081
Finlay Whittington-Devereux <fin.whittington1@gmail.com>
2020-03-13 12:36:43 +0000

    logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import { Link, withRouter } from 'react-router-dom'
+
+import logo from '../styles/images/IMGBIN_sound-loudness-png_Tn0RjnUj.png'
+
 // import LinearGradient from 'react-native-linear-gradient'
 // import MetisMenu from 'react-metismenu'
 // import Search from './Search'
@@ -40,7 +43,9 @@ class NavBar extends React.Component {
       > */}
       <div className="navbar-brand">
         <Link className="navbar-item" to="/">
-          <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
+
+          <img src={logo} width="112" height="28" />
+
         </Link>
 
         <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
